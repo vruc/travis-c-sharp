0ae369a28fa54aaf45aa1764aa5f229cbd4a58f8
Oscar.Chen <chanruida@outlook.com>
2016-02-15 07:17:15 +0000

    first commit

---
+++ Utility.Test/MathHelperTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyUtiliy;
+
+namespace Utility.Test
+{
+    [TestFixture]
+    public class MathHelperTest
+    {
+        [Test]
+        [TestCase(10,20)]
+        public void Add_Test_1(int x, int y)
+        {
+            Assert.AreEqual(MathHelper.Add(x, y), x + y);
+        }
+    }
+}
---
+++ Utiliy/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtiliy
+{
+    public static class MathHelper
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y + 1;
+        }
+
+        public static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
